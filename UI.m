2dfc53466796de8391b3480b033db65309b4ae66
jzhangbs <zhangjingyang33@126.com>
2018-02-18 20:48:30 +0800

    modify ui

--- UI.m
+++ UI.m
@@ -22,7 +22,7 @@ function varargout = UI(varargin)
 
 % Edit the above text to modify the response to help UI
 
-% Last Modified by GUIDE v2.5 06-Feb-2018 12:43:18
+% Last Modified by GUIDE v2.5 18-Feb-2018 14:03:24
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -71,3 +71,82 @@ function varargout = UI_OutputFcn(hObject, eventdata, handles)
 
 % Get default command line output from handles structure
 varargout{1} = handles.output;
+
+
+% --------------------------------------------------------------------
+function menu_file_Callback(hObject, eventdata, handles)
+% hObject    handle to menu_file (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --------------------------------------------------------------------
+function menu_tool_Callback(hObject, eventdata, handles)
+% hObject    handle to menu_tool (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --------------------------------------------------------------------
+function menu_scissor_Callback(hObject, eventdata, handles)
+% hObject    handle to menu_scissor (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --------------------------------------------------------------------
+function save_contour_Callback(hObject, eventdata, handles)
+% hObject    handle to save_contour (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --------------------------------------------------------------------
+function menu_save_mask_Callback(hObject, eventdata, handles)
+% hObject    handle to menu_save_mask (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on selection change in work_mode.
+function work_mode_Callback(hObject, eventdata, handles)
+% hObject    handle to work_mode (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns work_mode contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from work_mode
+
+
+% --- Executes during object creation, after setting all properties.
+function work_mode_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to work_mode (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --------------------------------------------------------------------
+function open_Callback(hObject, eventdata, handles)
+% hObject    handle to open (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+[f,p] = uigetfile({'*.jpg;*.png;*.bmp','Images (*.jpg,*.png,*.bmp)';
+                                 '*.*','All Files (*.*)'}, 'Select an image file');
+fp = fullfile(p, f);
+img = imread(fp);
+%axes(handles.axes1);
+imshow(img);
+
+
+% --- Executes on mouse press over axes background.
+function axes1_ButtonDownFcn(hObject, eventdata, handles)
+% hObject    handle to axes1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+pt = get(hObject,'CurrentPoint')
